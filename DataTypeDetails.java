f702b126c1262aa24170f175f425f4aac9d61b4f
anirudhreddy9966 <139291438+anirudhreddy9966@users.noreply.github.com>
2024-04-02 05:32:42 -0500

    Add files via upload

---
+++ DataTypeDetails.java
@@ -0,0 +1,22 @@
+
+import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class DataTypeDetails
+{
+   public static void main(String args[])
+   {
+    int b1=10;
+    int b2=-5;
+    //byte b3=012;
+    //byte b4=0XA;
+    //long l=9999999L;
+    //System.out.println(l);
+    System.out.println(Integer.toBinaryString(b1));
+    System.out.println(Integer.toBinaryString(b2));
+    //System.out.println(b3);
+    //System.out.println(b4);
+
+   }
+}
